d9cc385a3d63d9ce0741f18b75b976c37e3d95f6
Naomi Seyfer <seyfer@google.com>
2019-08-29 14:50:34 -0700

    Starting towards a slice interface

--- cmd/tools/interface_from_message.go
+++ cmd/tools/interface_from_message.go
@@ -6,7 +6,16 @@ import (
 	"io"
 	"reflect"
 	"strings"
+
+	"k8s.io/apimachinery/pkg/util/sets"
 )
+
+type GenerationOptions struct {
+	PkgPath    string
+	Roots      []reflect.Type
+	OmitFields map[string]sets.String
+	SliceKeys  map[string]sets.String
+}
 
 func WriteImports(imports map[string]string) string {
 	b := bytes.NewBufferString("")
@@ -22,9 +31,11 @@ func WriteImports(imports map[string]string) string {
 	return b.String()
 }
 
-func InterfacesToWrite(t reflect.Type, pkgPath string, imports map[string]string) map[reflect.Type]string {
+func InterfacesToWrite(tt []reflect.Type, pkgPath string, imports map[string]string) map[reflect.Type]string {
 	ret := map[reflect.Type]string{}
-	interfacesToWrite(t, pkgPath, imports, ret)
+	for _, t := range tt {
+		interfacesToWrite(t, pkgPath, imports, ret)
+	}
 	return ret
 }
 
@@ -33,6 +44,9 @@ func interfacesToWrite(t reflect.Type, pkgPath string, imports map[string]string
 		return
 	}
 	tm[t] = t.Name()
+	if t.Kind() != reflect.Struct {
+		return
+	}
 	for _, f := range TopLevelFields(t, imports) {
 		switch f.Type.Kind() {
 		case reflect.Map:
@@ -41,6 +55,9 @@ func interfacesToWrite(t reflect.Type, pkgPath string, imports map[string]string
 		case reflect.Ptr:
 			fallthrough
 		case reflect.Slice:
+			if f.Type.Elem().PkgPath() == pkgPath {
+				tm[f.Type] = f.Type.Elem().Name() + "Slice"
+			}
 			interfacesToWrite(f.Type.Elem(), pkgPath, imports, tm)
 		case reflect.Interface:
 			// pass
@@ -50,23 +67,131 @@ func interfacesToWrite(t reflect.Type, pkgPath string, imports map[string]string
 	}
 }
 
-func WriteInterface(t reflect.Type, imports map[string]string, tm map[reflect.Type]string) string {
+func WriteSliceInterface(t reflect.Type, imports map[string]string, tm map[reflect.Type]string, op GenerationOptions) string {
+	keys := op.SliceKeys[t.Elem().Name()]
+	fields := TopLevelFields(t.Elem(), imports)
+	var keyFields []reflect.StructField
+	if len(keys) > 0 {
+		keyFields = []reflect.StructField{}
+		for _, f := range fields {
+			if keys.Has(f.Name) {
+				keyFields = append(keyFields, f)
+			}
+		}
+	} else {
+		keyFields = fields
+	}
+
+	b := bytes.NewBufferString("")
+
+	fmt.Fprintf(b, "\n\ntype %sSlice interface {\n", t.Elem().Name())
+
+	// Iter()
+	fmt.Fprintf(b, "\tIter() chan ")
+	writeType(b, t.Elem(), imports, tm, false)
+	fmt.Fprintf(b, "\n")
+	// MatchesF for each key field
+	for _, f := range keyFields {
+		fmt.Fprintf(b, "\tMatches%s(%s ", f.Name, strings.ToLower(f.Name))
+		writeType(b, f.Type, imports, tm, false)
+		fmt.Fprintf(b, ") []")
+		writeType(b, t.Elem(), imports, tm, false)
+		fmt.Fprintf(b, "\n")
+	}
+	// Index(for all key fields) -1 is not present
+	// Get(for all key fields)
+	// Set(for all fields)
+	// Remove(for all key fields)
+	fmt.Fprintf(b, "}\n\n")
+	return b.String()
+}
+
+func WriteInterface(t reflect.Type, imports map[string]string, tm map[reflect.Type]string, op GenerationOptions) string {
 	b := bytes.NewBufferString("")
 	fmt.Fprintf(b, "type %s interface {\n", t.Name())
+	omit := op.OmitFields[t.Name()]
 	for _, f := range TopLevelFields(t, imports) {
+		if omit.Has(f.Name) {
+			continue
+		}
 		writeGetter(b, f, imports, tm)
+		switch f.Type.Kind() {
+		case reflect.Struct:
+		default:
+			writeSetter(b, f, imports, tm)
+		}
 	}
 	fmt.Fprintf(b, "}\n")
 	return b.String()
 }
 
+func typedefName(t reflect.Type, imports map[string]string) string {
+	switch t.Kind() {
+	case reflect.Struct:
+		pkgName := findPkgName(imports, t.PkgPath())
+		prefix := strings.Title(pkgName)
+		return prefix + t.Name()
+	case reflect.Slice:
+		pkgName := findPkgName(imports, t.Elem().PkgPath())
+		prefix := strings.Title(pkgName)
+		return prefix + t.Name() + "Slice"
+	}
+	panic("Got to the end and can't get a typedef name")
+	return ""
+}
+
+func WriteImplementation(t reflect.Type, imports map[string]string, tm map[reflect.Type]string, op GenerationOptions) string {
+	b := bytes.NewBufferString("")
+	fmt.Fprintf(b, "\n\ntype %s struct {\n\t*%s.%s\n}\n\n", typedefName(t, imports), findPkgName(imports, t.PkgPath()), t.Name())
+	omit := op.OmitFields[t.Name()]
+	for _, f := range TopLevelFields(t, imports) {
+		if omit.Has(f.Name) {
+			continue
+		}
+		writeGetterImpl(b, t, f, imports, tm)
+		switch f.Type.Kind() {
+		case reflect.Struct:
+		default:
+			writeSetterImpl(b, t, f, imports, tm)
+		}
+	}
+	return b.String()
+}
+
 func writeGetter(w io.Writer, f reflect.StructField, imports map[string]string, tm map[reflect.Type]string) {
-	fmt.Fprintf(w, "\t// Get %s\n\tGet%s() ", f.Name, f.Name)
-	writeType(w, f.Type, imports, tm)
+	fmt.Fprintf(w, "\tGet%s() ", f.Name)
+	writeType(w, f.Type, imports, tm, true)
 	w.Write([]byte("\n"))
 }
 
-func writeType(w io.Writer, t reflect.Type, imports map[string]string, tm map[reflect.Type]string) {
+func writeSetter(w io.Writer, f reflect.StructField, imports map[string]string, tm map[reflect.Type]string) {
+	fmt.Fprintf(w, "\tSet%s(o ", f.Name)
+	writeType(w, f.Type, imports, tm, false)
+	w.Write([]byte(")\n"))
+}
+
+func writeGetterImpl(w io.Writer, t reflect.Type, f reflect.StructField, imports map[string]string, tm map[reflect.Type]string) {
+	fmt.Fprintf(w, "func (r %s) Get%s() ", typedefName(t, imports), f.Name)
+	writeType(w, f.Type, imports, tm, true)
+	_, ok := tm[f.Type]
+	if ok {
+		fmt.Fprintf(w, " {\n\treturn %s{&r.%s}\n}\n", typedefName(f.Type, imports), f.Name)
+	} else if f.Type.Kind() == reflect.Struct {
+		fmt.Fprintf(w, " {\n\treturn &r.%s\n}\n", f.Name)
+	} else {
+		fmt.Fprintf(w, " {\n\treturn r.%s\n}\n", f.Name)
+	}
+}
+
+func writeSetterImpl(w io.Writer, t reflect.Type, f reflect.StructField, imports map[string]string, tm map[reflect.Type]string) {
+	fmt.Fprintf(w, "func (r %s) Set%s(o ", typedefName(t, imports), f.Name)
+	writeType(w, f.Type, imports, tm, false)
+	fmt.Fprintf(w, ") {\n")
+	fmt.Fprintf(w, "\tr.%s = o\n", f.Name)
+	fmt.Fprintf(w, "}\n")
+}
+
+func writeType(w io.Writer, t reflect.Type, imports map[string]string, tm map[reflect.Type]string, structStar bool) {
 	s, ok := tm[t]
 	if ok {
 		w.Write([]byte(s))
@@ -76,23 +201,25 @@ func writeType(w io.Writer, t reflect.Type, imports map[string]string, tm map[re
 	switch t.Kind() {
 	case reflect.Ptr:
 		w.Write([]byte("*"))
-		writeType(w, t.Elem(), imports, tm)
+		writeType(w, t.Elem(), imports, tm, false)
 	case reflect.Slice:
 		fmt.Fprintf(w, "[]")
-		writeType(w, t.Elem(), imports, tm)
+		writeType(w, t.Elem(), imports, tm, false)
 	case reflect.Map:
 		fmt.Fprintf(w, "map[")
-		writeType(w, t.Key(), imports, tm)
+		writeType(w, t.Key(), imports, tm, false)
 		fmt.Fprintf(w, "]")
-		writeType(w, t.Elem(), imports, tm)
+		writeType(w, t.Elem(), imports, tm, false)
+	case reflect.Struct:
+		// When we write structs as return values at least, we want to make them pointer-valued so
+		// we can do the usual thing of writing-through.
+		if structStar {
+			fmt.Fprintf(w, "*")
+		}
+		fallthrough
 	default:
 		if pkgPath != "" {
-			mapped, ok := tm[t]
-			if ok {
-				fmt.Fprintf(w, mapped)
-			} else {
-				fmt.Fprintf(w, "%s.%s", findPkgName(imports, pkgPath), t.Name())
-			}
+			fmt.Fprintf(w, "%s.%s", findPkgName(imports, pkgPath), t.Name())
 		} else {
 			w.Write([]byte(t.Name()))
 		}
@@ -131,6 +258,9 @@ func findPkgName(imports map[string]string, pkgPath string) string {
 		_, ok := reversed[candidate]
 		if !ok {
 			imports[pkgPath] = candidate
+			if candidate == "" {
+				panic(fmt.Sprintf("Empty package name for %v", pkgPath))
+			}
 			return candidate
 		}
 	}
--- cmd/tools/main.go
+++ cmd/tools/main.go
@@ -4,20 +4,60 @@ import (
 	"fmt"
 	"reflect"
 
+	"k8s.io/apimachinery/pkg/util/sets"
+
 	"knative.dev/serving/pkg/apis/serving/v1beta1"
 )
 
 func main() {
-	item := v1beta1.Service{}
-	imports := map[string]string{}
-	t := reflect.TypeOf(item)
-	tm := InterfacesToWrite(t, "knative.dev/serving/pkg/apis/serving/v1beta1", imports)
-	interfaces := []string{}
-	for tt, _ := range tm {
-		interfaces = append(interfaces, WriteInterface(tt, imports, tm))
+
+	nontemplateMetadataFields := sets.NewString("GenerateName", "Namespace", "SelfLink", "UID", "ResourceVersion", "Generation", "CreationTimestamp",
+		"DeletionTimestamp", "DeletionGracePeriodSeconds", "OwnerReferences", "Initializers", "Finalizers", "ClusterName")
+	op := GenerationOptions{
+		OmitFields: map[string]sets.String{
+			"RevisionSpec": sets.NewString("InitContainers", "RestartPolicy", "TerminationGracePeriodSeconds", "ActiveDeadlineSeconds", "DNSPolicy",
+				"NodeSelector", "DeprecatedServiceAccount", "AutomountServiceAccountToken", "NodeName", "HostNetwork", "HostPID",
+				"HostIPC", "ShareProcessNamespace", "SecurityContext", "ImagePullSecrets", "Hostname", "Subdomain", "Affinity",
+				"SchedulerName", "Tolerations", "HostAliases", "PriorityClassName", "Priority", "DNSConfig", "ReadinessGates",
+				"RuntimeClassName"),
+			"RevisionTemplateSpec": nontemplateMetadataFields,
+		},
+		PkgPath: "knative.dev/serving/pkg/apis/serving/v1beta1",
+		Roots: []reflect.Type{
+			reflect.TypeOf(v1beta1.Service{}),
+			reflect.TypeOf(v1beta1.Configuration{}),
+			reflect.TypeOf(v1beta1.Route{}),
+			reflect.TypeOf(v1beta1.Revision{}),
+		},
+		SliceKeys: map[string]sets.String{
+			"TrafficTarget": sets.NewString("Tag", "RevisionName", "LatestRevision"),
+		},
 	}
+	imports := map[string]string{}
+	tt := []reflect.Type{
+		reflect.TypeOf(v1beta1.Service{}),
+		reflect.TypeOf(v1beta1.Configuration{}),
+		reflect.TypeOf(v1beta1.Route{}),
+		reflect.TypeOf(v1beta1.Revision{}),
+	}
+	tm := InterfacesToWrite(tt, "knative.dev/serving/pkg/apis/serving/v1beta1", imports)
+	interfaces := []string{}
+	impls := []string{}
+	for t, _ := range tm {
+		switch t.Kind() {
+		case reflect.Struct:
+			interfaces = append(interfaces, WriteInterface(t, imports, tm, op))
+			impls = append(impls, WriteImplementation(t, imports, tm, op))
+		case reflect.Slice:
+			interfaces = append(interfaces, WriteSliceInterface(t, imports, tm, op))
+		}
+	}
+	fmt.Println("package generic\n")
 	fmt.Printf(WriteImports(imports))
 	for _, inter := range interfaces {
 		fmt.Printf(inter)
+	}
+	for _, impl := range impls {
+		fmt.Printf(impl)
 	}
 }
